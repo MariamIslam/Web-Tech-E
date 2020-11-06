e29c2d31fd9d12ff56120d88ff276c771120a7c3
MariamIslam <63836484+MariamIslam@users.noreply.github.com>
2020-11-07 00:00:49 +0600

    Add files via upload

---
+++ Labtask4/include.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+ <p align="right"><a href="Registration.php">Registration</a></p>
+<p align="right"><a href="https://www.w3schools.com/">Login</a></p>
+<p align="right"><a href="https://www.w3schools.com/">Home</a></p>
+<hr>
+</body>
+</html>
