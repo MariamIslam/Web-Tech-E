965c836cb82daab5e968689408630b915c9d02de
MariamIslam <63836484+MariamIslam@users.noreply.github.com>
2020-11-07 00:44:52 +0600

    Add files via upload

---
+++ Labtask4/ProfilePic.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<fieldset style="width:50%">
+<legend>Profile Picture</legend>
+<form>
+
+<img src="1024px-Circle-icons-profile.svg.png" alt="Flowers in Chania" width="260" height="145">
+<br>
+
+  upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</fieldset>
+<?php
+include 'footer.php';
+?>
+<hr>
+</body>
+</html>
